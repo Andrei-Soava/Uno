762bffb043ee5803c57098d1f7a1e2f21b475250
giuseppeluisi <g.luisi3@studenti.unibg.it>
2025-09-22 20:42:30 +0200

    inizializzazione del server
    creazione delle classi GameServer e ServerLaunch (serve per l'avvio del server)

---
+++ onegame-root/onegame-server/src/main/java/onegame/server/ServerLaunch.java
@@ -0,0 +1,18 @@
+package onegame.server;
+
+public class ServerLaunch {
+	//usato per l'avvio del server
+	public static void main(String[] args) {
+		int port = 9092;//porta di rete
+		GameServer gameserver = new GameServer(port);
+		gameserver.start();
+		
+		//mantiene sempre il server attivo
+		try {
+			Thread.sleep(Integer.MAX_VALUE);
+		}catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+}
