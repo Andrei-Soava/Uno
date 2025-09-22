c59115ff382f41f212650f8d8bfbdfd28654966e
LocatelliFabio <61050236+LocatelliFabio@users.noreply.github.com>
2025-09-22 15:10:12 +0200

    Rimosso ONEjava-original and update Mazzo tests
    Rimosso ONEjava-original module. Creati casi di test per Mazzo

--- onegame-root/onegame-common/src/test/java/modello/MazzoTest.java
+++ onegame-root/onegame-common/src/test/java/modello/MazzoTest.java
@@ -1,5 +1,7 @@
 package modello;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,20 +13,44 @@ import modello.carte.Colore;
 
 public class MazzoTest {
 	private Mazzo mazzo;
+	private Random random = new Random();
 	
 	@Before
     public void setup() {
         mazzo = new Mazzo();
+        mazzo.inizializzaNuovoMazzo();
     }
 
 	@Test
-    public void testaMescolamento() {
+    public void testInizializzaMazzo() {
         Carta c = mazzo.pesca();
-        assertEquals("Colore non corrisponde", Colore.ROSSO, c.getColore());
-
-        if (c instanceof CartaNumero) {
-            assertEquals("Numero non corrisponde", 0, ((CartaNumero) c).getNumero());
-        }
+        assertNotNull(c);
     }
-
+	
+	@Test
+	public void testSvuotaMazzo() {
+		mazzo.inizializzaNuovoMazzo();
+        assertFalse("Il mazzo è vuoto, ma dovrebbe essere pieno", mazzo.isVuoto());
+		mazzo.pescaN(mazzo.getNumeroCarte());
+		assertTrue("Il mazzo non è stato svuotato", mazzo.isVuoto());
+	}
+	
+	@Test
+	public void testPescaUnaCarta() {
+		mazzo.inizializzaNuovoMazzo();
+		int count1 = mazzo.getNumeroCarte();
+		mazzo.pesca();
+		int count2 = mazzo.getNumeroCarte();
+		assertTrue("Il pescaggio non funziona", count1 == count2 + 1);
+	}
+	
+	@Test
+	public void testPescaNCarte() {
+		mazzo.inizializzaNuovoMazzo();
+		int count1 = mazzo.getNumeroCarte();
+		int numeroCarteDaPescare = random.nextInt(count1);
+		mazzo.pescaN(numeroCarteDaPescare);
+		int count2 = mazzo.getNumeroCarte();
+		assertTrue("Il pescaggio non funziona", count1 == count2 + numeroCarteDaPescare);
+	}
 }
